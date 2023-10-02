25676eda3c402c37c0bfa65808266322236e2d30
shyamjayakannan <shyam.jayakannan@gmail.com>
2023-10-02 12:03:26 +0530

    css simple

--- frontend/src/components/home/heroHeaderSection.jsx
+++ frontend/src/components/home/heroHeaderSection.jsx
@@ -1,9 +1,11 @@
 "use client";
 
+import classes from "../../styles/home/heroHeaderSection.module.css";
+
 export default function HeroHeaderSection() {
     return (
         <section>
-            
+
         </section>
     );
 }
