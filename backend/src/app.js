0f9ab047ec1fe774293fb656d4edce49a9029ded
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-10-10 18:57:28 +0530

    dashboard

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,27 +11,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization,AuthToken"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization,AuthToken"
+  );
+  next();
 });
 
 routes.forEach((route) => app[route.method](route.path, route.handler));
 
 mongoose.set("strictQuery", false);
 mongoose
-    .connect(process.env.MONGODB_URL)
-    .then((result) => {
-        app.listen(8080);
-        console.log("Connected to 8080!");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.MONGODB_URL)
+  .then((result) => {
+    app.listen(8080);
+    console.log("Connected to 8080!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
