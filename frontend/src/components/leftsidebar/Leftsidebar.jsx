0d9bb82b50a7687dc1211076e76df2a958f2d86d
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-10-13 01:42:45 +0530

    dashboard

--- frontend/src/components/leftsidebar/Leftsidebar.jsx
+++ frontend/src/components/leftsidebar/Leftsidebar.jsx
@@ -1,13 +1,45 @@
-"use client"
-import classes from '../../styles/leftsidebar.module.css'
-
-const Leftsidebar =()=>{
-    return(
-        <>
-        <div >
-            mkc
-        </div>
-        </>
-    )
-}
+"use client";
+import { useContext, useEffect, useState } from "react";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import classes from "../../styles/leftsidebar.module.css";
+import useGetUser from "../../hooks/useGetUser";
+import AuthenticationContext from "../../store/AuthenticationContext";
+import Image from "next/image";
+const Leftsidebar = () => {
+  const [email, setemail] = useState("");
+  const getUser = useGetUser();
+  const authenticationCtx = useContext(AuthenticationContext);
+  useEffect(() => {
+    if (authenticationCtx.details.id === "") return;
+    async function wow() {
+      const set = await getUser(authenticationCtx.details);
+      setemail(set.email);
+      // console.log(await getUser(authenticationCtx.details))
+    }
+    wow();
+  }, [authenticationCtx.details]);
+  return (
+    <>
+      <div className={classes.main_section}>
+        <br />
+        <p className={classes.user}>
+          <Image
+            src="/images/avatar1.svg"
+            alt="avatar"
+            width={50}
+            height={50}
+            className={classes.avatar}
+          />{" "}
+          {email}
+        </p>
+        <p className={classes.buttons}>
+          <button className={classes.but}>Companies</button>
+          <button className={classes.but}>Students</button>
+          <button className={classes.but}>Shortlist</button>
+          <button className={classes.butset}>Settings</button>
+        </p>
+      </div>
+    </>
+  );
+};
 export default Leftsidebar;
