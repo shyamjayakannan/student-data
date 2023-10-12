423ff74a4e1ba9b81797796f7e5619f01304d2b0
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-10-13 02:02:54 +0530

    shyam changes

---
+++ frontend/src/store/ThemeContext.jsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useState, createContext, useEffect } from "react";
+
+const ThemeContext = createContext({
+    setTheme: () => {},
+    theme: String,
+});
+
+export function ThemeContextProvider({ children }) {
+    const [theme, setTheme] = useState();
+
+    useEffect(() => {
+        if (!theme) return setTheme(localStorage.getItem('theme'))
+
+        document.querySelector(':root').dataset.theme = theme
+        localStorage.setItem('theme', theme)
+
+        const useSetTheme = (e) => { setTheme(e.matches ? 'dark' : 'light') }
+        const watchSysTheme = window.matchMedia('(prefers-color-scheme: dark)')
+
+        watchSysTheme.addEventListener('change', useSetTheme);
+
+        function changeTheme() {
+            setTheme(localStorage.getItem('theme'));
+        }
+        window.addEventListener("storage", changeTheme);
+
+        return () => {
+            watchSysTheme.removeEventListener('change', useSetTheme);
+            removeEventListener("storage", changeTheme);
+        }
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider
+            value={{
+                setTheme,
+                theme,
+            }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeContext;
