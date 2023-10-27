1fe9b3a48e0de3ca72518a57050b58c6864958fa
Shubham Sharma <s_sharma2@me.iitr.ac.in>
2023-10-27 21:40:53 +0530

    popup

--- frontend/src/components/dashboard/Settings.jsx
+++ frontend/src/components/dashboard/Settings.jsx
@@ -2,10 +2,27 @@ import React, { useState } from "react";
 import classes from "../../styles/dashboard/dashboard.module.css";
 import Image from "next/image";
 
-import cal from '../../styles/settings.module.css'
+import cal from "../../styles/settings.module.css";
 
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={cal.modal}>
+      <div className={cal.modal_content}>{children}</div>
+    </div>
+  );
+};
 
 const Settings = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   let initialContents = [
     {
       name: "ML/AI",
@@ -35,7 +52,6 @@ const Settings = () => {
       name: "Management Trainee",
       check: false,
     },
-   
   ];
 
   const [contents, setContents] = useState([...initialContents]);
@@ -48,12 +64,14 @@ const Settings = () => {
       return item;
     });
     setContents(updatedContents);
+
     // console.log(contents)
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(contents);
+    setIsModalOpen(false);
   };
 
   return (
@@ -64,24 +82,37 @@ const Settings = () => {
         height={300}
         width={300}
       />
-      <p className={cal.text}>Select the category/categories of the companies:</p>
-      <form onSubmit={handleSubmit} >
-        <div className={cal.content}>
-          {contents.map((ele) => (
-            <span className={cal.inputs+' '+cal.grid_item} key={ele.name}>
-              <input
-                type="checkbox"
-                checked={ele.check}
-                className={cal.check}
-                name={ele.name}
-                onChange={() => handleCheck(ele.name)}
-              />
-              {ele.name}
-            </span>
-          ))}
-        </div>
-        <button type="submit" className={cal.done}>Done</button>
-      </form>
+      <p className={cal.text}>
+        Select the category/categories of the companies:
+      </p>
+
+      <button onClick={openModal} className={cal.open}>Categories</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <form onSubmit={handleSubmit}>
+          <div className={cal.content}>
+            {contents.map((ele) => (
+              <span className={cal.inputs + " " + cal.grid_item} key={ele.name}>
+                <input
+                  type="checkbox"
+                  checked={ele.check}
+                  className={cal.check}
+                  name={ele.name}
+                  onChange={() => handleCheck(ele.name)}
+                />
+                {ele.name}
+              </span>
+            ))}
+          </div>
+          <div className={cal.buts}>
+            <button type="submit" className={cal.done}>
+              Done
+            </button>
+            <button onClick={closeModal} className={cal.btn}>
+              Close
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
